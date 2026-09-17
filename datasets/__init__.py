2be8adcef888f5867900a4155a4d9ed9d38c3d19
Hoang <ndhoang2310@gmail.com>
2026-09-17 23:56:26 +0700

    fix: lazy import CoFlyModule to avoid patchify/sklearn dependency at startup

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,14 +3,15 @@ from typing import Dict
 import pytorch_lightning as pl
 
 from .pdc import PDCModule
-from .cofly import CoFlyModule
+
 
 def get_data_module(cfg: Dict) -> pl.LightningDataModule:
     dataset_name = cfg['data']['name']
-    if dataset_name == 'phenobench':  # phenobench'CWFID'
+    if dataset_name == 'phenobench':
         return PDCModule(cfg)
 
-    elif dataset_name == 'CoFly-WeedDB' or dataset_name == 'cofly':
+    elif dataset_name in ('CoFly-WeedDB', 'cofly'):
+        from .cofly import CoFlyModule  # lazy import: chỉ load khi thực sự cần
         return CoFlyModule(cfg)
 
     else:
